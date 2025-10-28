e051e010eaf0cc1120f1b1cfc8632fda21c203ea
mortalglitch <mortal.glitch@gmail.com>
2025-10-28 16:43:41 -0400

    finish ch3 L6 pushed solution changes due to a possible broken link

--- src/textnode.py
+++ src/textnode.py
@@ -12,18 +12,17 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, texttype, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = texttype
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if (self.text == other.text
-                and self.text_type == other.text_type
-                and self.url == other.url):
-            return True
-        else:
-            return False
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
@@ -44,3 +43,5 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img", "", {"alt": text_node.text, "src": text_node.url, })
     raise Exception("TextNode is an invalid type for processing")
+
+
